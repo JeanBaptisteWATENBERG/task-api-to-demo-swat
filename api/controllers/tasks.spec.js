4d996d27929caf7e78551d3c36d3644d6f01f51e
Jean-Baptiste WATENBERG <jeanbaptiste.watenberg@gmail.com>
2018-09-13 00:16:26 +0200

    add some tests

--- api/controllers/tasks.spec.js
+++ api/controllers/tasks.spec.js
@@ -7,11 +7,34 @@ describe('Tasks controller', () => {
 
     it('should create task delegate creation to repository', () => {
         //S 
-        req.swagger.params.body = {value: 'test'}
+        const expectedParameter = 'test';
+        const expectedResponse = 'createdTask';
+        req.swagger.params.body = {value: expectedParameter}
+        SUT.config.repository.insert.mockReturnValue(expectedResponse);
         //E
         SUT.createTasks(req, res);
         //V
-        expect(SUT.config.repository.insert).toHaveBeenCalledWith('test')
+        expect(SUT.config.repository.insert).toHaveBeenCalledTimes(1)
+        expect(SUT.config.repository.insert).toHaveBeenCalledWith(expectedParameter)
+        expect(jsonResMocked).toHaveBeenCalledTimes(1)
+        expect(jsonResMocked).toHaveBeenCalledWith(expectedResponse)
+    });
+
+    it ('should getTasks delegate get to repository', () => {
+        //S 
+        const expectedTitle = 'title';
+        const expectedStatus = 'status';
+        const expectedResponse = 'response';
+        req.swagger.params.title = {value: expectedTitle}
+        req.swagger.params.status = {value: expectedStatus}
+        SUT.config.repository.getByStatusAndTitle(expectedResponse);
+        //E
+        SUT.getTasks(req, res);
+        //V
+        expect(SUT.config.repository.getByStatusAndTitle).toHaveBeenCalledTimes(1)
+        expect(SUT.config.repository.getByStatusAndTitle).toHaveBeenCalledWith(expectedStatus, expectedTitle)
+        expect(jsonResMocked).toHaveBeenCalledTimes(1)
+        expect(jsonResMocked).toHaveBeenCalledWith(expectedResponse)
     });
 
 });
